3a38cbaadc699fca3c2a7dc7390074cd3874ffea
Caxer <webmaster@caxerx.com>
2025-12-24 21:37:19 +0900

    feat: add docker support

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   experimental: {
     inlineCss: true,
   },
+  output: "standalone",
 };
 
 export default nextConfig;
--- src/app/mint/layout.tsx
+++ src/app/mint/layout.tsx
@@ -1,9 +1,13 @@
 import { ServerProvider } from "@/provider/server-provider";
+import { notFound } from "next/navigation";
 
 export default function MintLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  if (!process.env.CONTRACT_NET) {
+    return notFound();
+  }
   return <ServerProvider>{children}</ServerProvider>;
 }
